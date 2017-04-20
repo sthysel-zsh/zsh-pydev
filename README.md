727f964d120f51b209f28e1c8a43f5f66ce30d68
Thys Meintjes <sthysel@gmail.com>
2017-04-20 22:32:12 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # zsh-pydev
+
 zsh plugin for Python development
+
+Provides:
+
+| Alias/Function | Description |
+| -------------- | ----------- |
+| pyfind         | Find .py files |
+| pygrep         | Search in python files |
+| pyclean        | Remove python cruft |
+
+Based on OMZ python plugin and others.
+
+# Install using zplug
+
+Using [zplug](https://github.com/zplug/zplug) in .zshrc 
+
+```
+zplug "sthysel/zsh-pydev"
+```
+
---
+++ pydev.zsh
@@ -0,0 +1,17 @@
+export WORKON_HOME=~/.virtualenvs
+source /usr/bin/virtualenvwrapper.sh
+
+# Find python file
+alias pyfind='find . -name "*.py"'
+
+# Grep among .py files
+alias pygrep='grep --include="*.py"'
+
+# Remove python compiled byte-code in either current directory or in a
+# list of specified directories
+function pyclean() {
+    ZSH_PYCLEAN_PLACES=${*:-'.'}
+    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
+    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
+}
+
